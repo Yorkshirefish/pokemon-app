ce1d7a9d6cf247d747431ab870996cb31ed208aa
Will Fisher <willem.fisher98@gmail.com>
2025-12-26 16:28:11 +0000

    Building Shiny Generator

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState}  from "react";
+import "../css/pokemon.css"
 
 function SinglePokemon({data}) {
+    const [pokeSprite, setPokeSprite] = useState('')
+    
 
-    console.log(data)
+    function isShiny() {
+        const randNum = Math.ceil(Math.random() * 100)
+
+        if(randNum === 1) {
+            setPokeSprite(data.sprites.front_shiny)
+        } else {
+            setPokeSprite(data.sprites.front_default)
+        }
+    }       
+
+    useEffect(() => {
+        isShiny();
+    },[])
+
+
 
     return (
         <div>
-            <img src={data.sprites.front_default} alt="" />
-            <h1>Name of Pokemon = {data.name}</h1>
+            <img src={pokeSprite} alt={data.name} />
+            <h1 className="pokemon-name">{data.name}</h1>
             <p></p>
         </div>
     )
