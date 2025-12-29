18951215edfbd10d7a52770037fa2b12603ff449
Will Fisher <willem.fisher98@gmail.com>
2025-12-29 22:01:15 +0000

    Adding App CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState} from "react";
 import PokemonTeam from "./components/pokemon-team";
+import "./css/app.css";
 
 function App() {
 
   return (
-    <div>
+    <div className="app-cont">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokemon Logo"/>
       <h1>Your Pokemon Team...</h1>
       <PokemonTeam />
     </div>
